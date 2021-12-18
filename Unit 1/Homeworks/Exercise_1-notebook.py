2deb4e1145f879b5e389dd3dcf663e489833eaf2
BennacerAmine <96210757+BennacerAmine@users.noreply.github.com>
2021-12-18 22:37:03 +0100

    Add files via upload

---
+++ Unit 1/Homeworks/Exercise_1-notebook.py
@@ -0,0 +1,8 @@
+#1
+list= range(2,299,2) #creating the list
+#2
+print(len(list)) #printing the length of the list
+for x in list :
+    print(x**2) #printing the quared values of the list
+#3
+print("57 is in the list :",57 in list) #checking if 57 is in the list
